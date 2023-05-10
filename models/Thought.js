1fcd5cbdd41ffd773f2846d17fa4a74330bf082a
ChrisJCota <chrisjocota@gmail.com>
2023-05-10 18:00:05 -0400

    models commit

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        //TO DO: need to add formating and getter for time created
+        get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a")
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema],
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    
+
+});
+
+thoughtSchema
+.virtual('reactionCount')
+.get(function () {
+    return `${this.reactions.length} `;
+  });
+
+  const Thoughts = model('user', thoughtSchema);
+
+  module.exports = Thoughts;
